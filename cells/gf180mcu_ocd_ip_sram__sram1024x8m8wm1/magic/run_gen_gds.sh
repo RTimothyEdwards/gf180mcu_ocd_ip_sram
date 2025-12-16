e45c9ec4d5c039a1c49d1e15842c2c317b687889
R. Timothy Edwards <tim@opencircuitdesign.com>
2025-12-16 18:59:00 -0500

    Added complete schematics for all three cells, which is critical for the 1kB SRAM because the 1kB SRAM does not have a vendor-supplied netlist.  All cells have been validated by LVS, although the 512 byte SRAM does not yet pass LVS against the schematic (it passes against the vendor-supplied netlist, therefore the layout is deemed valid).

---
+++ cells/gf180mcu_ocd_ip_sram__sram1024x8m8wm1/magic/run_gen_gds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Run layout GDS generation on the 1024B SRAM
+#
+echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
+echo ${PDK:=gf180mcuD} > /dev/null
+
+echo "Generating GDS for gf180mcu_ocd_ip_sram__sram1024x8m8wm1"
+magic -dnull -noconsole -rcfile ${PDK_ROOT}/${PDK}/libs.tech/magic/${PDK}.magicrc << EOF
+load gf180mcu_ocd_ip_sram__sram1024x8m8wm1
+select top cell
+gds write gf180mcu_ocd_ip_sram__sram1024x8m8wm1
+# NOTE:  Writing twice is necessary until the bug in magic that
+# causes hierarchical GDS areas to be missed on the first round
+# is found and fixed.
+gds write gf180mcu_ocd_ip_sram__sram1024x8m8wm1
+quit -noprompt
+EOF
+mv gf180mcu_ocd_ip_sram__sram1024x8m8wm1.gds ..
+echo "Done!"
